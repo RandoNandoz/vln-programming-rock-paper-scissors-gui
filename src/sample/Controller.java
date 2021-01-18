5d9ddf992d9d3e7beaef23f854b88cf582dda8f1
Randy Zhu <randyzhu@protonmail.com>
2021-01-18 14:23:40 -0800

    Add rps logic

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,11 +1,18 @@
 package sample;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+
+import java.security.SecureRandom;
 
 public class Controller {
     public Move currentPlayerMove;
     public ListView<Move> listMovesPlayable;
+    public Label labelSelectedMove;
+    public Label labelAIMove;
+    public Label labelAIScore;
+    public Label labelPlayerScore;
 
     public void initialize() {
         listMovesPlayable.getItems().add(new Move(Move.ROCK));
@@ -14,11 +21,47 @@ public class Controller {
     }
 
     public void doSelectCurrentOption() {
-        listMovesPlayable.getSelectionModel().getSelectedItem();
-        
+
     }
 
     public void buttonDoSelectedCurrentSelectedMove(ActionEvent actionEvent) {
+        currentPlayerMove = listMovesPlayable.getSelectionModel().getSelectedItem();
+    }
 
+    public void buttonDoPlayAction(ActionEvent actionEvent) {
+        SecureRandom secureRandom = new SecureRandom();
+        String aiMove = listMovesPlayable.getItems().get(secureRandom.nextInt(3)).getCurrentMove();
+        labelAIMove.setText(aiMove);
+        switch (aiMove) {
+            case Move.ROCK:
+                switch (currentPlayerMove.getCurrentMove()) {
+                    case Move.PAPER:
+                        labelPlayerScore.setText(String.valueOf(Integer.parseInt(labelPlayerScore.getText()) + 1));
+                    case Move.SCISSORS:
+                        labelAIScore.setText(String.valueOf(Integer.parseInt(labelAIScore.getText() + 1)));
+                    default:
+                        break;
+                }
+            case Move.PAPER:
+                switch (currentPlayerMove.getCurrentMove()) {
+                    case Move.ROCK:
+                        labelPlayerScore.setText(String.valueOf(Integer.parseInt(labelPlayerScore.getText()) + 1));
+                    case Move.SCISSORS:
+                        labelAIScore.setText(String.valueOf(Integer.parseInt(labelAIScore.getText()) + 1));
+                    default:
+                        break;
+                }
+            case Move.SCISSORS:
+                switch (currentPlayerMove.getCurrentMove()) {
+                    case Move.ROCK:
+                        labelPlayerScore.setText(String.valueOf(Integer.parseInt(labelPlayerScore.getText()) + 1));
+                    case Move.PAPER:
+                        labelAIScore.setText(String.valueOf(Integer.parseInt(labelPlayerScore.getText()) + 1));
+                    default:
+                        break;
+                }
+            default:
+                break;
+        }
     }
 }
