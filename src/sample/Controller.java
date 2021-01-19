1c68cacb4c3b40c6daf3ccedb1d57c48bcb23553
Randy Zhu <randyzhu@protonmail.com>
2021-01-18 16:04:43 -0800

    rps complete? binaries built

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -8,7 +8,7 @@ import javafx.scene.control.ListView;
 import java.security.SecureRandom;
 
 public class Controller {
-    public Move currentPlayerMove = null;
+    public Move currentPlayerMove;
     public ListView<Move> listMovesPlayable;
     public Label labelSelectedMove;
     public Label labelAIMove;
@@ -33,39 +33,48 @@ public class Controller {
         labelSelectedMove.setText(currentPlayerMove.getCurrentMove());
     }
 
-    public void buttonDoPlayAction(ActionEvent actionEvent) throws Exception {
+    public void buttonDoPlayAction(ActionEvent actionEvent) {
         SecureRandom secureRandom = new SecureRandom();
         String aiMove = listMovesPlayable.getItems().get(secureRandom.nextInt(3)).getCurrentMove();
         String playerMove = currentPlayerMove.getCurrentMove();
         labelAIMove.setText(aiMove);
-        if (aiMove.equals(Move.ROCK)) {
-            if (playerMove.equals(Move.ROCK)) {
-                return;
-            } else if (playerMove.equals(Move.PAPER)) {
-                labelPlayerScore.setText(String.valueOf(Integer.parseInt(labelPlayerScore.getText()) + 1));
-            } else if (playerMove.equals(Move.SCISSORS)) {
-                labelAIScore.setText(String.valueOf(Integer.parseInt(labelAIScore.getText()) + 1));
-            } else {
-                throw new Exception();
-            }
-        } else if (aiMove.equals(Move.PAPER)) {
-            if (playerMove.equals(Move.ROCK)) {
-                labelAIScore.setText(String.valueOf(Integer.parseInt(labelAIScore.getText()) + 1));
-            } else if (playerMove.equals(Move.PAPER)) {
-                return;
-            } else if (playerMove.equals(Move.SCISSORS)) {
-                labelPlayerScore.setText(String.valueOf(Integer.parseInt(labelPlayerScore.getText()) + 1));
-            } else {
-                throw new Exception();
-            }
-        } else if (aiMove.equals(Move.SCISSORS)) {
-            if (playerMove.equals(Move.ROCK)) {
-                labelPlayerScore.setText(String.valueOf(Integer.parseInt(labelPlayerScore.getText()) + 1));
-            } else if (playerMove.equals(Move.PAPER)) {
-                labelAIScore.setText(String.valueOf(Integer.parseInt(labelAIScore.getText()) + 1));
-            } else if (playerMove.equals(Move.SCISSORS)) {
-                return;
-            } else return;
+        switch (aiMove) {
+            case Move.ROCK:
+                switch (playerMove) {
+                    case Move.PAPER:
+                        labelPlayerScore.setText(String.valueOf(Integer.parseInt(labelPlayerScore.getText()) + 1));
+                        break;
+                    case Move.SCISSORS:
+                        labelAIScore.setText(String.valueOf(Integer.parseInt(labelAIScore.getText()) + 1));
+                        break;
+                    default:
+                        break;
+                }
+                break;
+            case Move.PAPER:
+                switch (playerMove) {
+                    case Move.ROCK:
+                        labelAIScore.setText(String.valueOf(Integer.parseInt(labelAIScore.getText()) + 1));
+                        break;
+                    case Move.SCISSORS:
+                        labelPlayerScore.setText(String.valueOf(Integer.parseInt(labelPlayerScore.getText()) + 1));
+                        break;
+                    default:
+                        break;
+                }
+                break;
+            case Move.SCISSORS:
+                switch (playerMove) {
+                    case Move.ROCK:
+                        labelPlayerScore.setText(String.valueOf(Integer.parseInt(labelPlayerScore.getText()) + 1));
+                        break;
+                    case Move.PAPER:
+                        labelAIScore.setText(String.valueOf(Integer.parseInt(labelAIScore.getText()) + 1));
+                        break;
+                    default:
+                        break;
+                }
+                break;
         }
         currentPlayerMove = null;
         labelSelectedMove.setText("");
